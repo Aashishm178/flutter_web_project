2089877ebc16c4c2a4a14f19af426e22dfef77c8
Aashishm178 <aashishc901@gmail.com>
2021-05-15 12:36:29 +0530

    [Initial] first-commit
    Signed-off-by: Aashishm178 <aashishc901@gmail.com>

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/aashish/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/media/aashish/9e8eec78-61f8-4921-8164-ba3715562e9f/Flutter projects/flutter_web_project"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
